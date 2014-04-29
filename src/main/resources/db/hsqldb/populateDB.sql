db40b358d2bd4f1a13f62bf15c6475dcbd34e40a
franzsch <franzschrader@gmx.net>
2014-04-29 14:33:54 +0200

    - made populadeDB.sql consistent with initDB.sql

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -1,8 +1,9 @@
-insert into users values(1,'Christian','Henle','chris','$2a$10$WVPq/GcSLNcKECb.wIfGeuFD0etGZpkgh6cgxdzmdzlBGdgp7Jzh2');
-insert into users values(2,'test','test','test','$2a$10$5GMyJxnb8405GyQ3rzhrQuke8DTtEoKJCq9Fhf8Y41zluFhPZa0Qi');
-insert into authorities values(1,'ROLE_ADMIN');
-insert into authorities values(2,'test');
-insert into authorities values(3,'ROLE_CUSTOMER');
-insert into user_authorities values(1,1);
-insert into user_authorities values(2,2);
-
+INSERT INTO users VALUES (1,'Christian','Henle','chris','$2a$10$WVPq/GcSLNcKECb.wIfGeuFD0etGZpkgh6cgxdzmdzlBGdgp7Jzh2');
+INSERT INTO users VALUES (2,'test','test','test','$2a$10$5GMyJxnb8405GyQ3rzhrQuke8DTtEoKJCq9Fhf8Y41zluFhPZa0Qi');
+INSERT INTO authorities VALUES (1,'ROLE_ADMIN');
+INSERT INTO authorities VALUES (2,'test');
+INSERT INTO authorities VALUES (3,'ROLE_CUSTOMER');
+INSERT INTO user_authorities VALUES (1,1);
+INSERT INTO user_authorities VALUES (2,2);
+INSERT INTO events VALUES (1, 'TestEvent', '2010-09-07');
+INSERT INTO locations VALUES (1, 'TestLocation', 1);

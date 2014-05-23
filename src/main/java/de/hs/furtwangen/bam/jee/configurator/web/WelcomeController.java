0e0747578f1005fb12a48d8ec8bcdfd7a24a8c6e
Christian Henle <Christian.Henle@gmx.de>
2014-05-23 10:08:07 +0100

    Spring MVC Config imported to business-config.xml, Spring Webflow Example added

---
+++ src/main/java/de/hs/furtwangen/bam/jee/configurator/web/WelcomeController.java
@@ -0,0 +1,20 @@
+package de.hs.furtwangen.bam.jee.configurator.web;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController
+{
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String intro(Model model)
+  {
+    model.addAttribute("date",new Date().toString());
+    return "home";
+  }
+}

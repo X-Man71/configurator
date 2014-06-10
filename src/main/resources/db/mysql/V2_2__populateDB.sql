c99c162d1c15f668eb8ca749e7718f1c8b65f0c5
Christian Henle <christian.henle@gmx.de>
2014-06-10 13:43:18 +0200

    Default Bank added

--- src/main/resources/db/mysql/V2_2__populateDB.sql
+++ src/main/resources/db/mysql/V2_2__populateDB.sql
@@ -4,6 +4,7 @@ INSERT INTO authority VALUES (1,'ROLE_CUSTOMER');
 INSERT INTO user_authority VALUES (1,1);
 INSERT INTO user_authority VALUES (2,1);
 
-INSERT INTO bank VALUES (1, 'Bank A', 5, 12, 500, 10000, 9600, 9999, 'asyncron', 'requestBankA');
-INSERT INTO bank VALUES (2, 'Bank B', 1, 24, 100, 10000, 9700, 9999, 'asyncron', 'requestBankB');
-INSERT INTO bank VALUES (3, 'VBank', 0, 1000, 0, 1000000, 500, 9999, 'syncron', 'default');
+INSERT INTO bank VALUES (1, 'keine Bank', 0, 0, 0, 1000000, 0, 0, 'syncron', 'default');
+INSERT INTO bank VALUES (2, 'Bank A', 5, 12, 500, 10000, 9600, 9999, 'asyncron', 'requestBankA');
+INSERT INTO bank VALUES (3, 'Bank B', 1, 24, 100, 10000, 9700, 9999, 'asyncron', 'requestBankB');
+INSERT INTO bank VALUES (4, 'VBank', 0, 0, 0, 1000000, 500, 9999, 'syncron', 'default');

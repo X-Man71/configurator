c84f8b64c141bcbcf718e8b66bbe5495e3ae93b0
franzsch <franzschrader@gmx.net>
2014-06-17 17:04:03 +0200

    - removed obsolete files

--- src/main/java/de/hs/furtwangen/bam/jee/configurator/model/events/orders/RequestAllOrdersEvent.java
+++
@@ -1,6 +0,0 @@
-package de.hs.furtwangen.bam.jee.configurator.model.events.orders;
-
-import de.hs.furtwangen.bam.jee.configurator.events.RequestReadEvent;
-
-public class RequestAllOrdersEvent extends RequestReadEvent {
-}

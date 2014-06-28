ccec795b928b39128c10f004bb99a255dbae5ccc
Christian Henle <christian.henle@gmx.de>
2014-06-28 11:45:13 +0200

    Admin added

--- src/main/resources/db/mysql/V2_2__populateDB.sql
+++ src/main/resources/db/mysql/V2_2__populateDB.sql
@@ -1,8 +1,13 @@
 INSERT INTO users VALUES (1,'Christian','Henle','christian','$2a$10$WA/gJRl61iVg6MbXSmvRNO3CqQm0gjSd5kllBhfca/2TaeUh4XiAy','christian@test.de',0,'x','2014-05-01','testA');
 INSERT INTO users VALUES (2,'Oliver','Roevekamp','oliver','$2a$10$8bokICydBJXRRvKLBJbNFe85t3gMyKkcwHs1Jy79NNTmbFXgN3CMW','asdf@asdf.de',0,'x','2014-05-01','testA');
+INSERT INTO users VALUES (3,'admin','admin','admin','$2a$10$WA/gJRl61iVg6MbXSmvRNO3CqQm0gjSd5kllBhfca/2TaeUh4XiAy','christian@test.de',0,'x','2014-05-01','testA');
+
 INSERT INTO authority VALUES (1,'ROLE_CUSTOMER');
+INSERT INTO authority VALUES (2,'ROLE_ADMIN');
 INSERT INTO user_authority VALUES (1,1);
 INSERT INTO user_authority VALUES (2,1);
+INSERT INTO user_authority VALUES (3,2);
+
 
 INSERT INTO bank VALUES (1, 'kein Angebot', 0, 0, 0, 1000000, 0, 0, 'syncron', 'default');
 INSERT INTO bank VALUES (2, 'Bank A', 5, 12, 500, 10000, 9600, 9999, 'asyncron', 'requestBankA');

be4d7f0b3f57cac83238c617c0da3df506d28b82
Christian Henle <Christian.Henle@gmx.de>
2014-08-12 16:50:52 +0200

    SpringSecurityIntegrationTest added. LoginPage added. StartPage added. Updated to Spring Security 4.0.0M1

--- src/main/java/de/hs/furtwangen/bam/jee/configurator/web/SecurityController.java
+++ src/main/java/de/hs/furtwangen/bam/jee/configurator/web/SecurityController.java
@@ -3,7 +3,6 @@ package de.hs.furtwangen.bam.jee.configurator.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * This Controller is responsible for mapping login Pages
@@ -12,17 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class SecurityController {
-	/**
-	 * Mappes login Page
-	 * 
-	 * @param model
-	 * @return html page login
-	 */
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(Model model) {
-		return "login";
-	}
-
+	
 	/**
 	 * Mappes the same Page as Login Method. In addition this Method add
 	 * {@link Model} which shows the User that the username and password were
